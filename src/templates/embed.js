ddaab69a450475d3fe26affba5b883cd92cde183
paul <ppymou@gmail.com>
2014-03-16 00:06:34 -0700

    Template

---
+++ src/templates/embed.js
@@ -0,0 +1,25 @@
+(function(cloverite) {
+    var _scripTagId = '{{SCRIPT_ID}}',
+    styleTag        = document.createElement('link');
+
+    styleTag.rel   = 'stylesheet';
+    styleTag.type  = 'text/css';
+    styleTag.href  = 'http://cloverite.com:9000/static/css/compiled.css';
+    styleTag.media = 'all';
+    document.getElementsByTagName('head')[0].appendChild(styleTag);
+
+    styleTag       = document.createElement('link');
+    styleTag.rel   = 'stylesheet';
+    styleTag.type  = 'text/css';
+    styleTag.href  = 'http://cloverite.com:9000/static/css/cleanslate.css';
+    styleTag.media = 'all';
+    document.getElementsByTagName('head')[0].appendChild(styleTag);
+
+    var div       = document.createElement('div');
+    div.id        = '{{DIV_ID}}';
+    div.className = 'cloverite-container cleanslate';
+    div.innerHTML = '{{{RENDERED}}}';
+
+    scriptTag = document.getElementsById(_scripTagId);
+    scriptTag.parentNode.insertBefore(div, scriptTag);
+})(this);
