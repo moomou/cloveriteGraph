25b88027d1d0d316bf8c4582f806419af4a793fc
paul <ppymou@gmail.com>
2014-03-18 06:43:38 +0000

    What

--- src/templates/embed.js
+++ src/templates/embed.js
@@ -15,9 +15,15 @@
     styleTag.media = 'all';
     document.getElementsByTagName('head')[0].appendChild(styleTag);
 
+    styleTag.rel   = 'stylesheet';
+    styleTag.type  = 'text/css';
+    styleTag.href  = '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css';
+    styleTag.media = 'all';
+    document.getElementsByTagName('head')[0].appendChild(styleTag);
+
     var div       = document.createElement('div');
     div.id        = '{{DIV_ID}}';
-    div.className = 'cleanslate cloverite-container';
+    div.className = 'cloverite-container cleanslate';
     div.innerHTML = '{{{RENDERED}}}';
 
     scriptTag = document.getElementById(_scripTagId);
